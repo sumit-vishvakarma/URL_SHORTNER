20682f442922a6cb82c4546849dbf48c9ef9ed56
sumit-vishvakarma <sumit-vishvakarma>
2025-09-24 19:25:31 +0530

    Add ShortUrlsController, routes, and basic URL submission form

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,10 +1,30 @@
 class ShortUrlsController < ApplicationController
   def new
+    @short_url = ShortUrl.new
   end
 
   def create
+    @short_url = ShortUrl.new(short_url_params)
+    if @short_url.save
+      redirect_to short_url_path(@short_url)
+    else
+      render :new
+    end
   end
 
   def show
+    @short_url = ShortUrl.find(params[:id])
+  end
+
+  # Redirect short code to original URL
+  def redirect
+    short_url = ShortUrl.find_by!(short_code: params[:short_code])
+    redirect_to short_url.long_url
+  end
+
+  private
+
+  def short_url_params
+    params.require(:short_url).permit(:long_url)
   end
 end
