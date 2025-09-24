9ba5e8a9025cb748318b84550a92fb9565eeec7f
sumit-vishvakarma <sumit-vishvakarma>
2025-09-24 19:22:59 +0530

    Add ShortUrl model with long_url, short_code and validation

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,2 +1,17 @@
 class ShortUrl < ApplicationRecord
+# Validations
+  validates :long_url, presence: true, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
+  validates :short_code, uniqueness: true
+
+  # Callbacks
+  before_create :generate_short_code
+
+  private
+
+  # Generate a unique short code
+  def generate_short_code
+    begin
+      self.short_code = SecureRandom.urlsafe_base64(6)
+    end while self.class.exists?(short_code: short_code)
+  end
 end
